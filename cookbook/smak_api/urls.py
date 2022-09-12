237b6cb6a7459dbcafbfbcd62b57319e1a96c2ca
andre <andre.slznv@gmail.com>
2022-09-12 14:19:48 +0300

    added urls for work with recipe images by API

--- cookbook/smak_api/urls.py
+++ cookbook/smak_api/urls.py
@@ -6,9 +6,17 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
-    path('', views.api_home, name='home'),
-    path('all/', views.DishesList.as_view(), name='all'),
+    path('', views.DishesList.as_view(), name='all'),
     path('dishes/<str:dish_name>/', views.DishDetail.as_view(), name='dish'),
+    
+    path('recipes/', views.RecipeList.as_view()),
+    path('recipes/<str:dish_name>/', views.RecipeDetail.as_view()),
+    
+    
+    path('recipes/image/add/', views.ImageCreate.as_view()),
+    path('recipes/image/<int:pk>/', views.ImageDetail.as_view()),
+    path('recipes/image/all/', views.ImageList.as_view()),
+    
     
 ]
 
